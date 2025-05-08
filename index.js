e5a4c0f16215d8a2e98cd63228d2e17d75dd443b
tictools <jaumevillarreal@gmail.com>
2025-05-08 19:03:50 +0200

    feat: change db structure and add new start script

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const jsonServer = require("json-server");
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+
 const PORT = process.env.PORT || 8080;
 
 server.use(middlewares);
